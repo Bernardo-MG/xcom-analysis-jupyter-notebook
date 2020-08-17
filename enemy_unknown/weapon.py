84e6e90bd3491aa830019a032f1f2879f964ac3e
Bernardo <programming@bernardomg.com>
2020-08-18 00:14:07 +0200

    Corrected imports

--- enemy_unknown/weapon.py
+++ enemy_unknown/weapon.py
@@ -8,13 +8,16 @@ def max_shots(group):
 
     if shots > group["capacity"]:
         # Shoots above capacity. Has to reload
-        max_shots = 100 / group["time_units"]
-        if (max_shots > 1):
+
+        # We have 100 TU. This is used to calculate the max number of shots
+        reload_shots = 100 / group["time_units"]
+        if reload_shots > 1:
             # Adds cost to reload between shots
-            reload_cost = (max_shots - 1) * 15
-            # The cost will be averaged to the shot cost
-            reload_cost = reload_cost / max_shots
+            reload_cost = (reload_shots - 1) * 15
+            # The cost will be averaged to each shot
+            reload_cost = reload_cost / reload_shots
         else:
+            # A single shot. No reload needed
             reload_cost = 0
         shots = 100 / (group["time_units"] + reload_cost) * group["burst"]
 
