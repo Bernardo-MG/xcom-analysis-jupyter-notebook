6d3725d24f8c29cd625a634eba7f0a7590c7f883
Bernardo <programming@bernardomg.com>
2020-08-18 00:41:02 +0200

    Removed meaningless decimals

--- enemy_unknown/weapon.py
+++ enemy_unknown/weapon.py
@@ -4,13 +4,14 @@ def max_shots(group):
 
     Takes into consideration that the weapon may have to reload.
     """
-    shots = 100 / group["time_units"] * group["burst"]
+    fire_actions = int(100 / group["time_units"])
+    shots = fire_actions * group["burst"]
 
     if shots > group["capacity"]:
         # Shoots above capacity. Has to reload
 
         # We have 100 TU. This is used to calculate the max number of shots
-        reload_shots = 100 / group["time_units"]
+        reload_shots = int(100 / group["time_units"])
         if reload_shots > 1:
             # Adds cost to reload between shots
             reload_cost = (reload_shots - 1) * 15
@@ -19,7 +20,9 @@ def max_shots(group):
         else:
             # A single shot. No reload needed
             reload_cost = 0
-        shots = 100 / (group["time_units"] + reload_cost) * group["burst"]
+        cost = group["time_units"] + reload_cost
+        fire_actions = int(100 / cost)
+        shots = fire_actions * group["burst"]
 
     return shots
 
